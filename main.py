c6fe7f5a1ae3ae1fdc72c9d59359f482260014d8
Ubuntu <ubuntu@ip-172-31-23-51.us-east-2.compute.internal>
2022-09-11 22:02:35 +0000

    Added execution code in main.py.

--- main.py
+++ main.py
@@ -50,7 +50,6 @@ def go(config: DictConfig):
         )
 
     if "check_data" in steps_to_execute:
-
         _ = mlflow.run(
             os.path.join(root_path, "check_data"),
             "main",
@@ -62,9 +61,17 @@ def go(config: DictConfig):
         )
 
     if "segregate" in steps_to_execute:
-
-        ## YOUR CODE HERE: call the segregate step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "segregate"),
+            "main",
+            parameters={
+                "input_artifact": "preprocessed_data.csv:latest",
+                "artifact_root": "data",
+                "artifact_type": "segregated_data",
+                "test_size": config["data"]["test_size"],
+                "stratify": config["data"]["stratify"]
+            },
+        )
 
     if "random_forest" in steps_to_execute:
 
@@ -74,13 +81,28 @@ def go(config: DictConfig):
         with open(model_config, "w+") as fp:
             fp.write(OmegaConf.to_yaml(config["random_forest_pipeline"]))
 
-        ## YOUR CODE HERE: call the random_forest step
-        pass
+       _ = mlflow.run(
+            os.path.join(root_path, "random_forest"),
+            "main",
+            parameters={
+                "train_data": "data_train.csv:latest",
+                "model_config": model_config,
+                "export_artifact": config["random_forest_pipeline"]["export_artifact"],
+                "random_seed": config["main"]["random_seed"],
+                "val_size": config["data"]["test_size"],
+                "stratify": config["data"]["stratify"]
+            },
+        )
 
     if "evaluate" in steps_to_execute:
-
-        ## YOUR CODE HERE: call the evaluate step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "evaluate"),
+            "main",
+            parameters={
+                "model_export": f"{config['random_forest_pipeline']['export_artifact']}:latest",
+                "test_data": "data_test.csv:latest"
+            },
+        )
 
 
 if __name__ == "__main__":
